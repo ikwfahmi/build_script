292bcb23a80ce2c2f269d938daf53d5b07cdf017
Kyura <240605110116@student.uin-malang.ac.id>
2025-07-02 21:01:39 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ rm -rf .repo/local_manifests/
 rm -rf prebuilts/clang/host/linux-x86
 
 #repo init
-repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs
+repo init -u https://github.com/HorizonV2/android.git -b lineage-22.2 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -29,4 +29,4 @@ echo "======= Export Done ======"
 echo "====== Envsetup Done ======="
 
 #build
-breakfast X00TD userdebug && make installclean && rise b
+lunch lineage_X00TD-bp1a-userdebug && make installclean && mka horizon
